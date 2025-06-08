a08516824c69d990afc3ea0966ecbe9ac5fab982
Nike073i <nike073i@mail.ru>
2025-06-08 21:23:43 +0400

    if x then y else null

--- select-maps/if-else-switch.sql
+++ select-maps/if-else-switch.sql
@@ -1,10 +1,19 @@
 SELECT 
     name,
     CASE
-        WHEN monthtly_maintenance < 100 THEN 'cheap'
+        WHEN monthly_maintenance < 100 THEN 'cheap'
         ELSE 'expensive'
     END cost
 FROM post_exers.facilities;
+
+-- Или, если подходит - Х, иначе - NULL
+SELECT 
+    start_time,
+    CASE
+        WHEN start_time >= '2012-09-02' THEN 'new'
+    END mark
+FROM post_exers.bookings
+WHERE DATE_TRUNC('month', start_time) = '2012-09-01';
 
 -- Operation with switch
 SELECT (b.slots * CASE WHEN m.id = 0 THEN f.guestcost ELSE f.membercost END) cost
