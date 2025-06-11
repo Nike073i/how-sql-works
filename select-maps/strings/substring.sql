49f892052639175f9e2a55fdefcef2b5a698d5b2
Nike073i <nike073i@mail.ru>
2025-06-12 00:12:02 +0400

    итерирование по строке

--- select-maps/strings/substring.sql
+++ select-maps/strings/substring.sql
@@ -1,3 +1,10 @@
 -- Обрезать текст с 2-ой буквы (включая) длиной в 5 символов. 
 -- Как я понял - применить к константам - нельзя
 SELECT SUBSTR(text_ex.t,2,5) FROM (SELECT 'This is text' t) text_ex; --his i
+
+-- Пример итератора по строке
+WITH 
+    seq AS (SELECT 'nikita' s),
+    iter AS (SELECT i FROM seq, generate_series(1, LENGTH(seq.s)) i)
+SELECT SUBSTR(seq.s, iter.i, 1) 
+    FROM iter, seq;
